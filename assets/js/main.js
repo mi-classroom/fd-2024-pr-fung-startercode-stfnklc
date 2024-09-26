a9c078bfcae380fd0f2e7409c92c9c84cef20060
Stefan Nikolic <stefan.nikolic@smail.th-koeln.de>
2024-09-26 13:51:36 +0200

    F10: Bilder aus dem Projekt groß anzeigen

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,26 +8,33 @@ document.addEventListener('DOMContentLoaded', function() {
   slideshow();
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() { 
   const galleryContainer = document.getElementById('gallery-container');
   const metadataUrl = '/works/n-pola/04-results/images/metadata.json'; 
 
-  
   fetch(metadataUrl)
     .then(response => response.json())
     .then(data => {
-     
       data.forEach(imageData => {
         const imageWrapper = document.createElement('div'); 
         imageWrapper.classList.add('gallery-item'); 
+
+        
+        const linkElement = document.createElement('a');
+        linkElement.href = `/works/n-pola/04-results/${imageData.src}`; 
+        linkElement.setAttribute('data-lightbox', 'gallery'); 
+        linkElement.setAttribute('data-title', imageData.metadata.Description); 
 
         const imageElement = document.createElement('img');
         imageElement.src = `/works/n-pola/04-results/${imageData.src}`; 
         imageElement.alt = imageData.metadata.Description; 
         imageElement.classList.add('gallery-image'); 
 
-       
-        imageWrapper.appendChild(imageElement);
+        
+        linkElement.appendChild(imageElement);
+
+        
+        imageWrapper.appendChild(linkElement);
 
         
         galleryContainer.appendChild(imageWrapper);
